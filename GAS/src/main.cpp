0448d5522b3c10a590c44ebfa81fe463a2606bbc
jaklys <machacek.jan2@gmail.com>
2023-08-15 18:07:04 +0000

    GAS V1.0

--- GAS/src/main.cpp
+++ GAS/src/main.cpp
@@ -6,22 +6,28 @@
 #define CAN_RX_PIN 44
 #define CAN_TX_PIN 43
 
+#define NODE_TYPE 0x11
+#define FIRMWARE_VERSION "1.0"
+
+int major, minor;
+
 unsigned long last_adc_reading_time[6] = {0};
 
 // interval mezi čteními vzorků z ADC
 const unsigned long ADC_READ_INTERVAL = 20; // 20 ms
 
-double input1, output1, setpoint1;
-double input2, output2, setpoint2;
-double input3, output3, setpoint3;
+double input1, output1, setpoint1, lastSetpoint1;
+double input2, output2, setpoint2, lastSetpoint2;
+double input3, output3, setpoint3, lastSetpoint3;
 
-double Kp = 2, Ki = 2, Kd = 0;
-
+double Kp = 1, Ki = 3, Kd = 0;
+const int pidSampleTime = 50;
+const int outputMin = 300;
 PID pidVent1(&input1, &output1, &setpoint1, Kp, Ki, Kd, P_ON_E, DIRECT);
 PID pidVent2(&input2, &output2, &setpoint2, Kp, Ki, Kd, P_ON_E, DIRECT);
 PID pidVent3(&input3, &output3, &setpoint3, Kp, Ki, Kd, P_ON_E, DIRECT);
 
-const int ADC_AVERAGE_MEASURE = 5;
+const int ADC_AVERAGE_MEASURE = 4;
 
 uint16_t pidForVent1 = 0;
 uint16_t pidForVent2 = 0;
@@ -29,8 +35,12 @@ uint16_t pidForVent3 = 0;
 
 enum GPIOToSend
 {
-  SOL1 = 10, // RELE 1
-  SOL2 = 18, // RELE 4
+  PR_GAS = 10,  // RELE 1
+  CUT_OXI = 11, // RELE 2
+  PR_OXI = 17,  // RELE 3
+  EXHAUST = 18, // RELE 4
+  HP_OXI = 16,  // RELE 5
+
   PWM_VENT_1 = 12,
   PWM_VENT_2 = 13,
   PWM_VENT_3 = 14,
@@ -78,14 +88,14 @@ enum MessageIDToRecieve
 {
   PWM_TARGET_ID = 0x287,
   ADC_VALUE_ID = 0x288,
-  MESSAGE3_ID = 0x500,
+  COMMAND_ID = 0x500,
 };
 
 enum MessageIDToSend
 {
   SEND_OUTPUT_ADC_ID = 0x2E0,
   SEND_INPUT_ADC_ID = 0x2E1,
-  COMMAND_RESPOND_ID = 0x400,
+  SEDN_COMMAND_RESPOND_ID = 0x400,
   SEDN_PID_COMPUTED_ID = 0x600,
 };
 
@@ -120,6 +130,14 @@ int readGPIO(int gpioPin)
 void setGPIO(int GPIO, bool state)
 {
   digitalWrite(GPIO, state ? HIGH : LOW);
+}
+
+void splitVersion(const char *version, int &major, int &minor)
+{
+  if (sscanf(version, "%d.%d", &major, &minor) != 2)
+  {
+    Serial.println("Chyba: Neplatný formát verze.");
+  }
 }
 
 MessageCommon *createMessageWithAllOnes()
@@ -279,12 +297,22 @@ MessageCommon *createMessage_COMMAND_RESPOND()
   message->byte0 = 0;
   message->byte1 = 0;
   message->byte2 = 0;
-  message->byte3 = 0;
-  message->byte4 = 0;
+  message->byte3 = major;
+  message->byte4 = minor;
   message->byte5 = 0;
   message->byte6 = 0;
   message->byte7 = 0;
   return message;
+}
+
+void handleCommand(int command)
+{
+  if (command == 0x01)
+  {
+    Serial.println("Get version");
+    Serial.println();
+    splitVersion(FIRMWARE_VERSION, major, minor);
+  }
 }
 
 uint16_t *extractValuesFromCANMessage(uint8_t canData[8])
@@ -310,7 +338,6 @@ uint16_t convertTo12Bit(uint16_t value)
 
 void setVents(int toSetpint1, int toSetpint2, int toSetpint3)
 {
-
   int adc_reading2 = ADC_FROM_CAN_2;
   int adc_reading4 = ADC_FROM_CAN_4;
   int adc_reading6 = ADC_FROM_CAN_6;
@@ -326,16 +353,42 @@ void setVents(int toSetpint1, int toSetpint2, int toSetpint3)
   if (setpoint1 == 0)
   {
     pidVent1.SetMode(MANUAL);
+    output1 = 0;
+  }
+  else
+  {
+    if (lastSetpoint1 == 0)
+    {
+      output1 = outputMin;
+    }
     pidVent1.SetMode(AUTOMATIC);
   }
+
   if (setpoint2 == 0)
   {
     pidVent2.SetMode(MANUAL);
+    output2 = 0;
+  }
+  else
+  {
+    if (lastSetpoint2 == 0)
+    {
+      output2 = outputMin;
+    }
     pidVent2.SetMode(AUTOMATIC);
   }
+
   if (setpoint3 == 0)
   {
     pidVent3.SetMode(MANUAL);
+    output3 = 0;
+  }
+  else
+  {
+    if (lastSetpoint3 == 0)
+    {
+      output3 = outputMin;
+    }
     pidVent3.SetMode(AUTOMATIC);
   }
 
@@ -350,17 +403,21 @@ void setVents(int toSetpint1, int toSetpint2, int toSetpint3)
 
   // Nastavení PWM výstupů pro každý ventil invertovaně
   Serial.println("-----------PWM1------------");
-  Serial.println(PWM_MAX - (int)output1);
+  Serial.println((int)output1);
   Serial.println();
   Serial.println("-----------PWM2------------");
-  Serial.println(PWM_MAX - (int)output2);
+  Serial.println((int)output2);
   Serial.println();
   Serial.println("-----------PWM3------------");
-  Serial.println(PWM_MAX - (int)output3);
+  Serial.println((int)output3);
   Serial.println();
-  ledcWrite(VENT_1_CHANNEL, setpoint1 == 0 ? PWM_MAX : PWM_MAX - (int)output1);
-  ledcWrite(VENT_2_CHANNEL, setpoint2 == 0 ? PWM_MAX : PWM_MAX - (int)output2);
-  ledcWrite(VENT_3_CHANNEL, setpoint3 == 0 ? PWM_MAX : PWM_MAX - (int)output3);
+  ledcWrite(VENT_1_CHANNEL, setpoint1 == 0 ? 0 : (int)output1);
+  ledcWrite(VENT_2_CHANNEL, setpoint2 == 0 ? 0 : (int)output2);
+  ledcWrite(VENT_3_CHANNEL, setpoint3 == 0 ? 0 : (int)output3);
+
+  lastSetpoint1 = setpoint1;
+  lastSetpoint2 = setpoint2;
+  lastSetpoint3 = setpoint3;
 }
 
 void handleCANMessage(int id, twai_message_t message)
@@ -384,8 +441,11 @@ void handleCANMessage(int id, twai_message_t message)
     Serial.println(values[2]);
     Serial.println();
     setVents(convertTo12Bit(values[0]), convertTo12Bit(values[1]), convertTo12Bit(values[2]));
-    setGPIO(SOL1, !((data[7] >> 0) & 0x01)); // získat logický stav 0. bitu
-    setGPIO(SOL2, !((data[7] >> 4) & 0x01)); // získat logický stav 4. bitu
+    setGPIO(CUT_OXI, ((data[7] >> 0) & 0x01));
+    setGPIO(PR_OXI, ((data[7] >> 1) & 0x01));
+    setGPIO(PR_GAS, ((data[7] >> 2) & 0x01));
+    setGPIO(HP_OXI, ((data[7] >> 3) & 0x01));
+    setGPIO(EXHAUST, ((data[7] >> 4) & 0x01));
 
     sendCANMessage(SEND_OUTPUT_ADC_ID, createMessage_SendOutPutADC(), 8);
     sendCANMessage(SEND_INPUT_ADC_ID, createMessage_SendInputADC(), 8);
@@ -407,7 +467,14 @@ void handleCANMessage(int id, twai_message_t message)
     ADC_FROM_CAN_6 = convertTo12Bit(values[2]);
     break;
 
-  case COMMAND_RESPOND_ID:
+  case COMMAND_ID:
+    Serial.print("-----------RecieveCOMMAND_ID------------");
+    Serial.println();
+    Serial.print("Command");
+    Serial.println(data[2]);
+    Serial.println();
+    handleCommand(data[2]);
+    sendCANMessage(SEDN_COMMAND_RESPOND_ID, createMessage_COMMAND_RESPOND(), 8);
     break;
   default:
     Serial.println("Neznama zprava");
@@ -419,14 +486,20 @@ void setup()
 {
   Serial.begin(115200);
 
-  digitalWrite(SOL1, HIGH);
-  digitalWrite(SOL2, HIGH);
-  pinMode(SOL1, OUTPUT);
-  pinMode(SOL2, OUTPUT);
+  digitalWrite(PR_GAS, LOW);
+  digitalWrite(CUT_OXI, LOW);
+  digitalWrite(PR_OXI, LOW);
+  digitalWrite(EXHAUST, LOW);
+  digitalWrite(HP_OXI, LOW);
+  pinMode(PR_GAS, OUTPUT);
+  pinMode(CUT_OXI, OUTPUT);
+  pinMode(PR_OXI, OUTPUT);
+  pinMode(EXHAUST, OUTPUT);
+  pinMode(HP_OXI, OUTPUT);
 
-  digitalWrite(PWM_VENT_1, HIGH);
-  digitalWrite(PWM_VENT_2, HIGH);
-  digitalWrite(PWM_VENT_3, HIGH);
+  digitalWrite(PWM_VENT_1, LOW);
+  digitalWrite(PWM_VENT_2, LOW);
+  digitalWrite(PWM_VENT_3, LOW);
   pinMode(PWM_VENT_1, OUTPUT);
   pinMode(PWM_VENT_2, OUTPUT);
   pinMode(PWM_VENT_3, OUTPUT);
@@ -448,25 +521,28 @@ void setup()
   ledcSetup(VENT_3_CHANNEL, PWM_FREQUENCY_PWM3, PWM_RESOLUTION);
   ledcAttachPin(PWM_VENT_3, VENT_3_CHANNEL);
 
-  ledcWrite(VENT_1_CHANNEL, PWM_MAX);
-  ledcWrite(VENT_2_CHANNEL, PWM_MAX);
-  ledcWrite(VENT_3_CHANNEL, PWM_MAX);
+  ledcWrite(VENT_1_CHANNEL, 0);
+  ledcWrite(VENT_2_CHANNEL, 0);
+  ledcWrite(VENT_3_CHANNEL, 0);
 
   pidVent1.SetMode(AUTOMATIC);
   pidVent1.SetOutputLimits(0, PWM_MAX);
   pidVent1.SetControllerDirection(DIRECT);
+  pidVent1.SetSampleTime(pidSampleTime);
 
   pidVent2.SetMode(AUTOMATIC);
   pidVent2.SetOutputLimits(0, PWM_MAX);
   pidVent2.SetControllerDirection(DIRECT);
+  pidVent2.SetSampleTime(pidSampleTime);
 
   pidVent3.SetMode(AUTOMATIC);
   pidVent3.SetOutputLimits(0, PWM_MAX);
   pidVent3.SetControllerDirection(DIRECT);
+  pidVent3.SetSampleTime(pidSampleTime);
 
-  setpoint1 = PWM_MAX;
-  setpoint2 = PWM_MAX;
-  setpoint3 = PWM_MAX;
+  setpoint1 = 0;
+  setpoint2 = 0;
+  setpoint3 = 0;
 
   twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)CAN_TX_PIN, (gpio_num_t)CAN_RX_PIN, TWAI_MODE_NO_ACK); // TWAI_MODE_NORMAL, TWAI_MODE_NO_ACK or TWAI_MODE_LISTEN_ONLY
   twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
@@ -511,17 +587,26 @@ uint16_t recalculateADCinRange(int adcPin) // prepocet hodnoty z ADC do rozsahu 
   const int numReadings = ADC_AVERAGE_MEASURE;
   int total = 0; // součet všech čtení
 
-  for(int i = 0; i < numReadings; i++) {
+  for (int i = 0; i < numReadings; i++)
+  {
     uint16_t reading = analogRead(adcPin);
-    
-    if (reading < 375) {
+
+    if (reading < 375)
+    {
       reading = 0;
-    } else {
+    }
+    else
+    {
       reading = map(reading, 375, 3516, 0, 4095); // 375 je 0.5V, 3516 je 4.51V, vse v rozsahu napeti 0 - 0.5V je vyhodnoceno jako 0
     }
 
     total += reading;
+
+    Serial.print(" reading: ");
+    Serial.print(reading);
   }
+
+  Serial.println(" ");
 
   return total / numReadings; // průměrování čtení
 }
